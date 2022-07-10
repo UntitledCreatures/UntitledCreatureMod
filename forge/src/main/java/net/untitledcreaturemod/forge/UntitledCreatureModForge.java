9e584fb9317c549068065d9daedba5b9581a4400
Okabintaro <103938900+Okabintaro@users.noreply.github.com>
2022-07-10 22:55:12 +0200

    feat(Toad): Initial setup for forge

--- forge/src/main/java/net/untitledcreaturemod/forge/UntitledCreatureModForge.java
+++ forge/src/main/java/net/untitledcreaturemod/forge/UntitledCreatureModForge.java
@@ -1,12 +1,46 @@
 package net.untitledcreaturemod.forge;
 
+import net.minecraft.client.render.entity.EntityRenderers;
+import net.minecraft.entity.EntityType;
+import net.minecraftforge.common.ForgeMod;
+import net.minecraftforge.event.entity.EntityAttributeCreationEvent;
+import net.minecraftforge.eventbus.api.EventPriority;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 import net.untitledcreaturemod.UntitledCreatureMod;
 import net.minecraftforge.fml.common.Mod;
+import net.untitledcreaturemod.setup.ModEntities;
+import net.untitledcreaturemod.toad.ToadEntity;
+import net.untitledcreaturemod.toad.ToadRenderer;
+import software.bernie.geckolib3.GeckoLib;
 
 @Mod(UntitledCreatureMod.MOD_ID)
 public class UntitledCreatureModForge {
     public UntitledCreatureModForge() {
-        // Submit our event bus to let architectury register our content on the right time
         UntitledCreatureMod.init();
+        GeckoLib.initialize();
+        ModSetup.ENTITIES.register(FMLJavaModLoadingContext.get().getModEventBus());
+    }
+
+    @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
+    public static class ModSetup {
+        private static final DeferredRegister<EntityType<?>> ENTITIES = DeferredRegister.create(ForgeRegistries.ENTITIES, UntitledCreatureMod.MOD_ID);
+        public static final RegistryObject<EntityType<ToadEntity>> TOAD = ENTITIES.register("duck", ModEntities.TOAD);
+
+        @SubscribeEvent(priority = EventPriority.LOWEST)
+        public static void onRegisterAttributes(final EntityAttributeCreationEvent event) {
+            event.put(TOAD.get(), ToadEntity.getDefaultAttributes().add(ForgeMod.SWIM_SPEED.get(), 2.0D).build());
+        }
+
+        @SubscribeEvent
+        public static void clientSetup(FMLClientSetupEvent event) {
+            // TODO: Check if this work on severside
+            EntityRenderers.register(TOAD.get(), ToadRenderer::new);
+        }
+
     }
 }
---
+++ forge/src/main/java/net/untitledcreaturemod/forge/UntitledCreatureModForgeClient.java
@@ -0,0 +1,7 @@
+package net.untitledcreaturemod.forge;
+
+public class UntitledCreatureModForgeClient {
+    public static void setupEntityRenderers() {
+
+    }
+}
